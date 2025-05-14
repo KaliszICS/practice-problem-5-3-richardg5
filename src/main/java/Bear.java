3a12d46ad0c7440bf63c143a7840311ef8807e48
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-05-14 18:41:42 +0000

    Add code

---
+++ src/main/java/Bear.java
@@ -0,0 +1,15 @@
+public class Bear extends Animal {
+    public Bear(String name, int age, String type, int position) {
+        super(name, age, type, position);
+    }
+
+    @Override
+    public String animalSound() {
+        return "Roar";
+    }
+
+    @Override
+    public void move() {
+        position++;
+    }
+}
